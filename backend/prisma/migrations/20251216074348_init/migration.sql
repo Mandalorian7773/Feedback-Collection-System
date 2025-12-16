9a927b18e8f91d9f7d764505a8ae4c1113f035d7
Aditya Jagrani <adityajagrani@Adityas-MacBook-Air-3.local>
2025-12-16 19:16:55 +0530

    minor deployment fix

--- backend/prisma/migrations/20251216074348_init/migration.sql
+++ backend/prisma/migrations/20251216074348_init/migration.sql
@@ -1,13 +1,12 @@
 -- CreateTable
-CREATE TABLE "User" (
-    "id" SERIAL NOT NULL,
+CREATE TABLE "feedback" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "name" TEXT,
+    "phone" TEXT NOT NULL,
+    "rating" INTEGER NOT NULL,
+    "feedback" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "feedback_pkey" PRIMARY KEY ("id")
 );
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
